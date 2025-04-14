bb7289b8578eb75439159e97338b162e094a8778
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-14 16:24:22 +0530

    add logic to get dashboard for logged in and not logged in user.

--- app/routes/customer-dashboard.tsx
+++ app/routes/customer-dashboard.tsx
@@ -1,14 +1,29 @@
+import { getProfile } from "services/profile";
 import type { Route } from "./+types/customer-dashboard";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
   const { customerId } = params;
-  const;
+  if (customerId) {
+    const token = localStorage.getItem("loggedInUser");
+    if (token) {
+      const user = await getProfile(customerId, token);
+      console.log(user);
+      return user;
+    } else {
+      console.log("token not found");
+    }
+  }
+  return null;
 }
 
-export default function CustomerDashboard() {
+export default function CustomerDashboard({
+  loaderData,
+}: Route.ComponentProps) {
+  const user = loaderData;
   return (
     <div>
-      <h1>Customer dashboard</h1>
+      {user && <div>you are logged in as {user.username}</div>}
+      {!user && <div>you are not signed in</div>}
     </div>
   );
 }
