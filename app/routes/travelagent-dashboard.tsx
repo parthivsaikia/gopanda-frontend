5f5a806e07e19c3fb6fe81ae24bedc5c79ef015b
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-14 16:25:45 +0530

    add logic for different dashboards

--- app/routes/travelagent-dashboard.tsx
+++ app/routes/travelagent-dashboard.tsx
@@ -1,3 +1,26 @@
-export default function AgentDashboard() {
-  return <div>Agent Dashboard</div>;
+import { getProfile } from "services/profile";
+import type { Route } from "./+types/travelagent-dashboard";
+
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const { agentId } = params;
+  if (!agentId) {
+    return null;
+  }
+  const token = localStorage.getItem("loggedInUser");
+  if (!token) {
+    return null;
+  }
+  const user = await getProfile(agentId, token);
+  console.log(user);
+  return user;
+}
+
+export default function AgentDashboard({ loaderData }: Route.ComponentProps) {
+  const user = loaderData;
+  return (
+    <div>
+      {user && <div>logged in as {user.username}</div>}
+      {!user && <div>not logged in</div>}
+    </div>
+  );
 }
