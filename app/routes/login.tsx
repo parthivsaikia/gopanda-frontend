747b249c709e94ff3248bf88d9fe502c3b394158
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-14 12:59:12 +0530

    add logic to get username and id

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,37 +1,30 @@
-import { Form, Link } from "react-router";
+import { Form, Link, redirect } from "react-router";
 import type { Route } from "./+types/login";
 import axios from "axios";
 import { login } from "services/login";
-import { act, useEffect } from "react";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export async function clientAction({
   params,
   request,
 }: Route.ClientActionArgs) {
   try {
-    let error: string = "";
     const formData = await request.formData();
     const username = formData.get("username") as string;
     const password = formData.get("password") as string;
     const user = await login(username, password);
-    console.log(username, password);
-    console.log(user);
-    return { user, error };
+    const data = jwtDecode(user.token);
+    localStorage.setItem("loggedInUser", user.token);
+    return redirect(
+      data.role === "TravelAgent" ? "/agent-dashboard" : "/customer-dashboard"
+    );
   } catch (error) {
-    return { user: null, error: "Wrong credentials" };
+    return { error: "Wrong Credentials" };
   }
 }
 
 export default function LoginPage({ actionData }: Route.ComponentProps) {
-  if (actionData?.user && typeof window !== undefined) {
-    useEffect(() => {
-      window.localStorage.setItem(
-        "loggedInUser",
-        JSON.stringify(actionData.user)
-      );
-    }, [actionData]);
-  }
-
   return (
     <div>
       <h1>Login to use gopandas.</h1>
