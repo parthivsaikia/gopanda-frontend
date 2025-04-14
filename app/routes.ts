ef0cd94aea06f85ca146fd6cc1d0b6d680495991
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-14 13:00:34 +0530

    add route to customer dashboard and agent dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -3,5 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("auth/login", "routes/login.tsx"),
-  route("auth/signup", "routes/signup.tsx")
+  route("auth/signup", "routes/signup.tsx"),
+  route("customer-dashboard/:customerId", "routes/customer-dashboard.tsx"),
+  route("agent-dashboard/:agentId", "routes/travelagent-dashboard.tsx"),
 ] satisfies RouteConfig;
