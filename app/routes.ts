8a650e3c4e813a52691123fee64f3719991e7f13
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-15 16:15:48 +0530

    add logout route

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,15 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  layout,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("auth/login", "routes/login.tsx"),
   route("auth/signup", "routes/signup.tsx"),
-  route("customer-dashboard/:customerId?", "routes/customer-dashboard.tsx"),
-  route("agent-dashboard/:agentId?", "routes/travelagent-dashboard.tsx"),
+  route("customer-dashboard/", "routes/customer-dashboard.tsx"),
+  route("agent-dashboard/", "routes/travelagent-dashboard.tsx"),
+  route("logout", "routes/logout.tsx"),
 ] satisfies RouteConfig;
