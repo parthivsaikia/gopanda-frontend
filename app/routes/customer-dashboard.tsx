838a0d3bc75e6469e8dc28bcaf6427411d5a583a
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-15 16:02:34 +0530

    remove params from customer-dashboard.

--- app/routes/customer-dashboard.tsx
+++ app/routes/customer-dashboard.tsx
@@ -1,29 +1,41 @@
 import { getProfile } from "services/profile";
 import type { Route } from "./+types/customer-dashboard";
+import { Form } from "react-router";
+import { jwtDecode } from "jwt-decode";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  const { customerId } = params;
-  if (customerId) {
+  try {
     const token = localStorage.getItem("loggedInUser");
     if (token) {
-      const user = await getProfile(customerId, token);
-      console.log(user);
-      return user;
-    } else {
-      console.log("token not found");
+      const userData = jwtDecode(token);
+      const userId = userData.sub;
+      if (userId) {
+        const user = await getProfile(userId, token);
+        return { user };
+      }
     }
+    return { error: "token not found" };
+  } catch (error) {
+    return { error };
   }
-  return null;
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
 }
 
 export default function CustomerDashboard({
   loaderData,
 }: Route.ComponentProps) {
-  const user = loaderData;
+  const { user, error } = loaderData;
   return (
     <div>
+      <Form method="post" action="/logout">
+        <button>log out</button>
+      </Form>
       {user && <div>you are logged in as {user.username}</div>}
       {!user && <div>you are not signed in</div>}
+      {error && <div>{"some error happened"}</div>}
     </div>
   );
 }
