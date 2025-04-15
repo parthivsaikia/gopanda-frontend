972a42d4fb97004473c6c207a8d497e0c973a3a6
parthivsaikia <parthivsaikia985@gmail.com>
2025-04-15 16:15:26 +0530

    removed params logic from dashboard

--- app/routes/travelagent-dashboard.tsx
+++ app/routes/travelagent-dashboard.tsx
@@ -1,24 +1,35 @@
 import { getProfile } from "services/profile";
+import { Form } from "react-router";
 import type { Route } from "./+types/travelagent-dashboard";
+import { jwtDecode } from "jwt-decode";
 
 export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  const { agentId } = params;
-  if (!agentId) {
-    return null;
+  try {
+    const token = localStorage.getItem("loggedInUser");
+    if (token) {
+      const userData = jwtDecode(token);
+      const userId = userData.sub;
+      if (userId) {
+        const user = await getProfile(userId, token);
+        return user;
+      }
+    }
+  } catch (error) {
+    console.log(error);
   }
-  const token = localStorage.getItem("loggedInUser");
-  if (!token) {
-    return null;
-  }
-  const user = await getProfile(agentId, token);
-  console.log(user);
-  return user;
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
 }
 
 export default function AgentDashboard({ loaderData }: Route.ComponentProps) {
   const user = loaderData;
   return (
     <div>
+      <Form method="post" action="/logout">
+        <button>Log out</button>
+      </Form>
       {user && <div>logged in as {user.username}</div>}
       {!user && <div>not logged in</div>}
     </div>
